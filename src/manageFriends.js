6d724ebcf6212f6afde27d27de439d929fe15dd7
Philip Levy <philipdlevy@gmail.com>
2022-08-24 12:32:35 -0600

    commit tree e1a6b2e2f3c3df8c7a4b4ade1c415c6b6a049e55

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,47 @@
+import { managePresents } from "./managePresents";
+const initialState = {friends: []}
+
+
 export function manageFriends(state, action) {
+  // console.log("state", state)
+  console.log("action.payload", action.payload)
   // your code here
+  switch (action.type) {
+    case 'friends/add':
+      return {friends: [...state.friends, action.payload]}
+    case 'friends/remove':
+      return {friends: state.friends.filter((friend) => friend.id != action.payload)}
+    default:
+      return state;
+  }
 }
+
+
+
+// const initialState = { friends: [] };
+
+// let action = {
+//   type: 'friends/add',
+//   payload: {
+//     name: 'Mac Miller',
+//     hometown: 'Arizona',
+//     id: 1,
+//   },
+// };
+
+// function manageFriends(state, action) {
+//   switch (action.type) {
+//     case 'friends/add':
+//       return { friends: [...state.friends, action.payload] };
+//     case 'friends/remove':
+//       return {
+//         friends: state.friends.filter((friend) => friend.id != action.payload),
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+
+
+// manageFriends(initialState, action);
